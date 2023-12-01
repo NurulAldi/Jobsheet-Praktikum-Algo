92baae6b336bd0d3276386ba0386b526dbc78cb8
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-01 22:11:01 +0700

    Update Jobsheet 8 nomor 1 Praktikum algo.cpp

--- Jobsheet 8/Jobsheet 8 nomor 1 Praktikum algo.cpp
+++ Jobsheet 8/Jobsheet 8 nomor 1 Praktikum algo.cpp
@@ -1,12 +1,12 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include <stdio.h>
 
 // fungsi untuk melakukan perkalian menggunakan fungsi rekursif
-int multiply(int a, int b) {
+int perkalian(int a, int b) {
     if (b == 1) {
         return a;
     } else {
-        return a + multiply(a, b - 1); // menambahkan a dengan a sebanyak b kali 
+        return a + perkalian(a, b - 1); // menambahkan a dengan a sebanyak b kali 
     }
 }
 
@@ -21,10 +21,10 @@ int main() {
     scanf("%d", &num2);
 
 	// blok kode if else dibawah untuk memastikan apakah angka yang diinput user 0 kebawah atau tidak
-    if (num1 <= 0 || num2 <= 0) {
+    if (num1 <= 0 || num2 <= 0) { // jika angka pertama dan angka kedua sama atau lebih kecil dari 0
         printf("Masukkan hanya bilangan bulat positif diatas 0.\n");
-    } else {
-        int result = multiply(num1, num2);
+    } else { // jika angka pertama dan angka kedua besar dari 0
+        int result = perkalian(num1, num2); // memanggil fungsi perkalian
         printf("Hasil perkalian %d x %d adalah: %d\n", num1, num2, result);
     }
 
