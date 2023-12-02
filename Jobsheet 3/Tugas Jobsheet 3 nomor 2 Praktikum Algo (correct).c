9ccc43fa62716d6a77614f0a31dc2c226704d601
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:18:56 +0700

    Update Tugas Jobsheet 3 nomor 2 Praktikum Algo (correct).c

--- Jobsheet 3/Tugas Jobsheet 3 nomor 2 Praktikum Algo (correct).c
+++ Jobsheet 3/Tugas Jobsheet 3 nomor 2 Praktikum Algo (correct).c
@@ -1,31 +1,30 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 #include<conio.h>
 
 int main(){
 	float kelvin, fahrenheit, reamur, celcius;
 	
-	
+	// pengguna diminta menginputkan nilai suhu dalam bentuk celcius
 	printf("input suhu celcius : ");
-	// masukkan suhu dalam celcius
 	scanf("%f", &celcius);
 	
 	// operasi matematika untuk mengkonversi derajat celcius ke derajat kelvin
 	kelvin = celcius + 273.15;
 	
-	// menampilkan hasil konversi derajat celcius ke derajat fahrenheit
+	// menampilkan hasil konversi derajat celcius ke derajat kelvin dengan 2 angka dibelakang koma (%.2f)
 	printf("\nSuhu dalam kelvin : %.2f ", kelvin);
 	
 	// operasi matematika untuk mengkonversi derajat celcius ke derajat fahrenheit
 	fahrenheit = (celcius * 1.8) + 32;
 	
-	// menampilkan hasil konversi derajat celcius ke derajat fahrenheit
+	// menampilkan hasil konversi derajat celcius ke derajat fahrenheit dengan 2 angka dibelakang koma (%.2f)
 	printf("\nSuhu dalam fahrenheit : %.2f ", fahrenheit);
 	
 	// operasi matematika untuk mengkonversi derajat celcius ke derajat reamur
 	reamur = celcius * 0.8;
 	
-	// menampilkan hasil konversi derajat celcius ke derajat fahrenheit
+	// menampilkan hasil konversi derajat celcius ke derajat reamur dengan 2 angka dibelakang koma (%.2f)
 	printf("\nSuhu dalam reamur : %.2f", reamur);
 	
 	return 0;
