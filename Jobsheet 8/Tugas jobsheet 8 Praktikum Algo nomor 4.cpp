b0a6c95d3b92ae7a49691b35827e63e6ac92f5e8
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 09:46:28 +0700

    Update Tugas jobsheet 8 Praktikum Algo nomor 4.cpp

--- Jobsheet 8/Tugas jobsheet 8 Praktikum Algo nomor 4.cpp
+++ Jobsheet 8/Tugas jobsheet 8 Praktikum Algo nomor 4.cpp
@@ -1,32 +1,32 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
-double diskon;
+double diskon; // deklarasi variabel diskon 
 
 int potong(int ttl_belanja){
 
 	// blok kode if else dibawah digunakan untuk menentukan besaran diskon
-	if(ttl_belanja < 1000000){
+	if(ttl_belanja < 1000000){ // jika total belanja yang diinputkan user dibawah 1jt
 		printf("\nMaaf! Anda tidak mendapatkan diskon");
-	} else if(ttl_belanja <= 3000000){
-		diskon = 0.2;
+	} else if(ttl_belanja <= 3000000){ // jika total belanja yang diinputkan user dibawah 3jt
+		diskon = 0.2; // diskon 20%
 		printf("\nBesar diskon : %.1f%%", diskon *100);
-	} else{
-		diskon = 0.35;
+	} else{ // jika total belanja yang diinputkan user diatas 3jt
+		diskon = 0.35; // diskon 35%
 		printf("\nBesar diskon : %.1f%%", diskon*100);
 	}
 	
 	// Menampilkan harga total setelah dipotong diskon
-	printf("\nBesar harga yang harus dibayarkan : Rp.%.lf\n", ttl_belanja * (1 - diskon));
+	printf("\nBesar harga yang harus dibayarkan : Rp.%.lf\n", ttl_belanja * (1 - diskon)); // melakukan operasi matematika untuk menentukan harga dipotong diskon sekaligus menampilkannya
 	
 }
 
 int main(){
 	int total_belanja;
-	printf("Total pembelian : Rp.");
+	printf("Total pembelian : Rp."); // user diminta menginputkan total belanja untuk menentukan besaran diskon yang akan didapatkan
 	scanf("%d", &total_belanja);
 	
-	potong(total_belanja);
+	potong(total_belanja); // memanggil fungsi potong() yang akan menentukan harga setelah dipotong diskon
 	
 	return 0;
 }
