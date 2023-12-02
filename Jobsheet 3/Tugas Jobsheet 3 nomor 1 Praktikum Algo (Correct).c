7fb3757ae8498ef1d2d8f66ef12f682e016af1e9
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:16:47 +0700

    Update Tugas Jobsheet 3 nomor 1 Praktikum Algo (Correct).c

--- Jobsheet 3/Tugas Jobsheet 3 nomor 1 Praktikum Algo (Correct).c
+++ Jobsheet 3/Tugas Jobsheet 3 nomor 1 Praktikum Algo (Correct).c
@@ -1,16 +1,17 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	float rp, dollar;
-	
-	printf("Masukkan jumlah uang dalam rupiah : ");
+	float rp, dollar; deklarasi variabel rp untuk menyimpan jumlah uang dalam rupiah dan dollar untuk menyimpan hasil konversi rupiah ke dollar
+
 	// Pengguna menginputkan nilai uang dalam bentuk rupiah
-    scanf("%f", &rp);
+	printf("Masukkan jumlah uang dalam rupiah : ");
+  	scanf("%f", &rp);
 	
 	// operasi matematika untuk mengkonversi rupiah ke dollar
-    dollar = rp / 14250;
-    // menampilkan hasil konversi rupiah ke dollar
+    	dollar = rp / 14250;
+    
+	// menampilkan hasil konversi rupiah ke dollar
 	printf("\nJumlah uang anda dalam dollar adalah : %.2f$", dollar);
 	
 	return 0;
