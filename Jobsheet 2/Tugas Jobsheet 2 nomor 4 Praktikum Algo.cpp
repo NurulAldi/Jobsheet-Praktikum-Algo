085301c91bd7f40e79549f03fe66f8cc59ab501f
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:14:16 +0700

    Update Tugas Jobsheet 2 nomor 4 Praktikum Algo.cpp

--- Jobsheet 2/Tugas Jobsheet 2 nomor 4 Praktikum Algo.cpp
+++ Jobsheet 2/Tugas Jobsheet 2 nomor 4 Praktikum Algo.cpp
@@ -1,4 +1,4 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
@@ -7,12 +7,12 @@ int main(){
 	// lp = lebar persegi panjang
 	double lp = 5;
 	// lpp = luas persegi panjang
-	float lpp;
+	float lpp; // variable lpp digunakan untuk menyimpan nilai luas persegi panjang
 	
-	// menghitung luas persegi panjang
+	// menghitung luas persegi panjang menggunakan operasi matematika panjang * lebar
 	lpp = pp*lp;
 	// menampilkan luas persegi panjang
-	printf("Luas persegi panjang adalah : %.2f meter", lpp);
+	printf("Luas persegi panjang adalah : %.2f meter", lpp); // %.2f artinya 2 nol dibelakang koma (float adalah bilangan berkoma)
 	
 	return 0;
 	
