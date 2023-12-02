c3175b1e9b998879c454287e54245320b8b299c0
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:34:15 +0700

    Update Jobsheet 4 nomor 3 Praktikum algo.cpp

--- Jobsheet 4/Jobsheet 4 nomor 3 Praktikum algo.cpp
+++ Jobsheet 4/Jobsheet 4 nomor 3 Praktikum algo.cpp
@@ -1,17 +1,17 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	// deklarasi variabel nilai
-	int nilai_presensi = 85;
-	int nilai_praktek = 65;
-	int nilai_uas = 75;
-	int nilai_uts = 80;
-	float nilai_akhir;
+	// deklarasi variabel 
+	int nilai_presensi = 85; // deklarasi variabel nilai_presensi dengan nilai 85 dan tipe data integer
+	int nilai_praktek = 65; // deklarasi variabel nilai_praktek dengan nilai 65 dan tipe data integer
+	int nilai_uas = 75; // deklarasi variabel nilai_uas dengan nilai 75 dan tipe data integer
+	int nilai_uts = 80; // deklarasi variabel nilai_uts dengan nilai 80 dan tipe data integer
+	float nilai_akhir; // deklarasi variabel nilai_akhir dengan tipe data float
 	
 	// operasi matematika untuk menghitung nilai akhir 
 	nilai_akhir = (nilai_presensi*0.10)+(nilai_praktek*0.20)+(nilai_uas*0.40)+(nilai_uts*0.30);
-	printf("Nilai akhir adalah : %.2f", nilai_akhir);
+	printf("Nilai akhir adalah : %.2f", nilai_akhir); // menampilkan nilai akhir dengan 2 angka dibelakang koma 
 	
 	return 0;
 }
