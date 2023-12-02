76e21036e2024f1ceea02c9d0c41b5c53bf088ed
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:00:28 +0700

    Update Tugas jobsheet 9 nomor 3 b Praktikum Algo.cpp

--- Jobsheet 9/Tugas jobsheet 9 nomor 3 b Praktikum Algo.cpp
+++ Jobsheet 9/Tugas jobsheet 9 nomor 3 b Praktikum Algo.cpp
@@ -1,19 +1,22 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
 	// deklarasi variabel disertai valuenya 
 	int Lesley = 57082;
+	
 	// pointer Layla menunjuk ke alamat memori lesley
 	// sehingga pointer layla mengambil value dari variabel Lesley
-	int *Layla = &Lesley; 
+	int *Layla = &Lesley;
+	
 	// balmond memiliki value pointer layla (yang mengambil value variabel Lesley) + 1
 	int Balmond = *Layla + 1;
 	
 	// menampilkan alamat memori karena tidak menggunakan tanda alamat memori '&'
 	// akan menampilkan value lesley jika menggunakan tanda '&' sebelum variabel 'Layla'
 	printf("Nilai Layla adalah : %d", Layla); 
-	
+
+	// menampilkan nilai balmond yaitu 57083 ( nilai variabel pointer Layla + 1 )
 	printf("\nNilai Balmond adalah : %d", Balmond);
 		
 }
