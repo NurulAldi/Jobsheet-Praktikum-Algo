b2cddb54a20ed7ce95deb727dd1d23bd333a73a5
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:24:34 +0700

    Update Jobsheeet 4 nomor 4 Praktikum Algo.cpp

--- Jobsheet 4/Jobsheeet 4 nomor 4 Praktikum Algo.cpp
+++ Jobsheet 4/Jobsheeet 4 nomor 4 Praktikum Algo.cpp
@@ -1,17 +1,17 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
 	// deklarasi variabel
-	int tarif_1jam = 15000;
-	int tarif_jamber = 0.5 * tarif_1jam;
-	int jam_tambahan = 2;
-	int total_bayar;
+	int tarif_1jam = 15000; // deklarasi nilai variabel tarif_1jam
+	int tarif_jamber = 0.5 * tarif_1jam; // deklarasi nilai variabel tarif_jamber menggunakan operasi perkalian matematika
+	int jam_tambahan = 2; // deklarasi nilai variabel jam_tambahan
+	int total_bayar; // deklarasi variabel total_bayar untuk menyimpan nilai total yang harus dibayarkan
 	
 	// operasi matematika untuk menghitung total yang harus dibayarkan 
-	total_bayar = tarif_1jam + (jam_tambahan * tarif_jamber);
+	total_bayar = tarif_1jam + (jam_tambahan * tarif_jamber); 
 	
-	// menampilkan total bayar 
+	// menampilkan total yang harus dibayarkan 
 	printf("Total bayar adalah : Rp.%i", total_bayar);
 	
 	return 0;
