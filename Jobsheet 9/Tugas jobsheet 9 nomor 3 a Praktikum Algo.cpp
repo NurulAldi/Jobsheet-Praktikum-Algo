925c009ab83048308c1e5f083908b75d84483ae0
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 09:57:50 +0700

    Update Tugas jobsheet 9 nomor 3 a Praktikum Algo.cpp

--- Jobsheet 9/Tugas jobsheet 9 nomor 3 a Praktikum Algo.cpp
+++ Jobsheet 9/Tugas jobsheet 9 nomor 3 a Praktikum Algo.cpp
@@ -1,11 +1,11 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
 	// deklarasi variabel disertai valuenya 
 	int Lesley = 57082;
-	int Layla = Lesley;
-	int Balmond = Layla + 1;
+	int Layla = Lesley; // nilai Layla sama dengan nilai Lesley
+	int Balmond = Layla + 1; // nilai balmond sama dengan nilai layla + 1 sehingga 57082 + 1 = 57083 (nilai balmond)
 	
 	// menampilkan value dari variabel Layla dan variabel Balmond
 	printf("Nilai Layla adalah : %d", Layla);
