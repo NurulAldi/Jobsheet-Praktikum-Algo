c3bbb26cba34eaf4c373cf91595d50ce25c58ea0
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:07:41 +0700

    Update Tugas Jobsheet 2 nomor 2 Praktikum Algo.c

--- Jobsheet 2/Tugas Jobsheet 2 nomor 2 Praktikum Algo.c
+++ Jobsheet 2/Tugas Jobsheet 2 nomor 2 Praktikum Algo.c
@@ -1,42 +1,42 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	char nama[50], prodi[50], fakultas[50];
+	char nama[50], prodi[50], fakultas[50]; // deklarasi variabel string dengan panjang karakter maksimal 50 karakter
 	int nim;
-	double NUA, NP, NUT, NA;
+	double NUA, NP, NUT, NA; 
 	
-	// pengguna menginput nama
+	// User diminta untuk menginputkan nama (panjang karakter maksimal 50 karakter)
 	printf("Nama : ");
 	scanf("%s", &nama);
 	
-	// pengguna menginput NIM
+	// User diminta untuk menginputkan nim (berupa bilangan bulat atau dengan kata lain integer(int))
 	printf("NIM : ");
 	scanf("%i", &nim);
 	
-	// pengguna menginput Prodi
+	// User diminta untuk menginputkan prodi (panjang karakter maksimal 50 karakter)
 	printf("Prodi : ");
 	scanf("%s", &prodi);
 	
-	// pengguna menginput Fakultas
+	// User diminta untuk menginputkan fakultas (panjang karakter maksimal 50 karakter)
 	printf("Fakultas : ");
 	scanf("%s", &fakultas);
 	
-	// pengguna menginput Nilai Praktikum
+	// User diminta untuk menginputkan Nilai Praktikum (boleh bilangan bulat boleh bilangan berkoma atau dengan kata lain double)
 	printf("Nilai Praktikum : ");
 	scanf("%d", &NP);
 	
-	// pengguna menginput nilai UTS
+	// User diminta untuk menginputkan UTS (boleh bilangan bulat boleh bilangan berkoma atau dengan kata lain double)
 	printf("Nilai UTS : ");
 	scanf("%d", &NUT);
 	
-	// pengguna menginput nilai UAS
+	// User diminta untuk menginputkan Nilai UAS (boleh bilangan bulat boleh bilangan berkoma atau dengan kata lain double)
 	printf("Nilai UAS : ");
 	scanf("%d", &NUA);
 	
 	// operasi matematika untuk menghitung nilai akhir 
 	NA = (0.3*NP) + (0.3*NUT) + (0.4*NUA);
-	// menampilkan nilai akhir pengguna
+	// menampilkan nilai akhir user
 	printf("Nilai Akhir : %d", NA);
 	
 	return 0;
