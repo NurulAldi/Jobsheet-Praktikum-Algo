3b156fce82ed780b76942187b086cf2c129a4f00
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:21:12 +0700

    Update Tugas Jobsheet 3 nomor 3 Praktikum Algo (correct).c

--- Jobsheet 3/Tugas Jobsheet 3 nomor 3 Praktikum Algo (correct).c
+++ Jobsheet 3/Tugas Jobsheet 3 nomor 3 Praktikum Algo (correct).c
@@ -1,22 +1,22 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	int diameter = 12;
-	int r = 6;
-	float phi = 3.14;
-	float volume, luas_permukaan;
+	int diameter = 12; // deklarasi nilai variabel diameter
+	int r = 6; // deklarasi nilai variabel r
+	float phi = 3.14; // deklarasi nilai variabel phi
+	float volume, luas_permukaan; // deklarasi variabel volume dan luas_permukaan untuk menyimpan nilai volume dan nilai luas permukaan dari bola basket
 	
-	// operasi matematika menghitung volume bola basket
+	// operasi matematika untuk menghitung volume bola basket
 	volume =  (phi*r*r*r)*4/3;
 	
-	// menampilkan volume bola basket
+	// menampilkan volume bola basket dengan 2 angka dibelakang koma (%.2f)
 	printf("Volume bola basket adalah : %.2fm3", volume);
 	
 	// operasi matematika menghitung luas permukaan bola basket 
 	luas_permukaan = 4*phi*r*r;
 	
-	// menampilkan luas permukaan bola basket
+	// menampilkan luas permukaan bola basket dengan 2 angka dibelakang koma (%.2f)
 	printf("\nLuas permukaan bola basket adalah : %.2fm2", luas_permukaan);
 	
 	return 0;
