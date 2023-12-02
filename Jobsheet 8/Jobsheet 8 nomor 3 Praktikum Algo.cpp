95cae525eab098a6f2444dcd3ccfe09b9e03d374
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 09:39:49 +0700

    Update Jobsheet 8 nomor 3 Praktikum Algo.cpp

--- Jobsheet 8/Jobsheet 8 nomor 3 Praktikum Algo.cpp
+++ Jobsheet 8/Jobsheet 8 nomor 3 Praktikum Algo.cpp
@@ -1,18 +1,18 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
-float phi = 3.14;
+float phi = 3.14; // deklarasi nilai phi 3,14 karena sudah merupakan ketetapan
 
 // fungsi untuk menentukan keliling lingkaran
 int keliling(float a){
-	printf("\nKeliling lingkaran adalah : %.2f cm", 2*phi*a);
-	return 0;
+	printf("\nKeliling lingkaran adalah : %.2f cm", 2*phi*a); // menampilkan sekaligus melakukan operasi matematika untuk menentukan keliling lingkaran
+	return 0; // fungsi mengembalikan nilai integer
 }
 
 // fungsi untuk menentukan luas lingkaran
 int luas(float a){
-	printf("\nLuas lingkaran adalah : %.2f cm2", phi*a*a);
-	return 0;
+	printf("\nLuas lingkaran adalah : %.2f cm2", phi*a*a); // menampilkan sekaligus melakukan operasi matematika untuk menentukan luas lingkaran
+	return 0; // fungsi mengembalikan nilai integer
 }
 
 int main(){
@@ -21,8 +21,8 @@ int main(){
 	printf("Inputkan jari jari lingkaran : "); // user diminta menginputkan jari jari lingkaran
 	scanf("%f", &r);
 	
-	keliling(r);
-	luas(r);
+	keliling(r); // pemanggilan fungsi yang menentukan keliling() lingkaran yang sudah dideklarasikan di atas
+	luas(r); // pemanggilan fungsi yang menentukan luas() lingkaran yang sudah dideklarasikan di atas 
 	
-	return 0;
+	return 0; 
 }
