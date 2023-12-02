735099d48c17f1a8d10f5ee132bbb636ad06754e
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-02 10:02:03 +0700

    Update Tugas Jobsheet 2 nomor 1 Praktikum Algo.cpp

--- Jobsheet 2/Tugas Jobsheet 2 nomor 1 Praktikum Algo.cpp
+++ Jobsheet 2/Tugas Jobsheet 2 nomor 1 Praktikum Algo.cpp
@@ -1,12 +1,12 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	char nama[100];
+	char nama[100]; // deklarasi nama dengan panjang maksimal 100 karakter 
 	
 	// Pengguna disuruh menginput nama lengkap dirinya
 	printf("Hello, siapa nama lengkapmu?\n");
-	scanf("%s", &nama);
+	scanf("%s", &nama); // fungsi scanf() untuk memberikan user kesempatan untuk menginputkan sesuatu
 	
 	// menampilkan kalimat dengan nama pengguna diantaranya
 	printf("\nSelamat datang %s dalam pemrograman C!", nama);
