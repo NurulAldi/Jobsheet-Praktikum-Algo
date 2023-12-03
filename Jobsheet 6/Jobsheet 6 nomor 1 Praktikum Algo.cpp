bdf90a366557fe69c9d2e746a3389c7e8347d166
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-03 09:39:30 +0700

    Update Jobsheet 6 nomor 1 Praktikum Algo.cpp

--- Jobsheet 6/Jobsheet 6 nomor 1 Praktikum Algo.cpp
+++ Jobsheet 6/Jobsheet 6 nomor 1 Praktikum Algo.cpp
@@ -1,24 +1,24 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 
 int main(){
-	
+	// inisialisasi variabel i
 	int i = 0;
 	
 	printf("deret bilangan genap :");
 	// for loop untuk menampilkan bilangan genap
 	for(int ab = 2; ab <= 50; i++){ 
 
-		printf("%i ", ab);
-		ab += 2;
+		printf("%i ", ab); // menampilkan nilai ab
+		ab += 2; // nilai ab akan selalu bertambah 2 selama memenuhi kondisi ab <= 50 
 		}
 	
 	printf("\nderet bilangan ganjil :");
 	// for loop untuk menampilkan bilangan ganjil
 	for(int ac = 1; ac <=50; i++){
 		
-		printf("%i ", ac);
-		ac += 2;
+		printf("%i ", ac); // menampilkan nilai ac
+		ac += 2; // nilai ab akan selalu bertambah 2 selama memenuhi kondisi ab <= 50
 	}
 	
 	return 0;
