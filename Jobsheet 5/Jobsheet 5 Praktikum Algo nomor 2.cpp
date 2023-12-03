4c68324198af8adc5c533997640db050e8e15caa
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-03 09:36:21 +0700

    Update Jobsheet 5 Praktikum Algo nomor 2.cpp

--- Jobsheet 5/Jobsheet 5 Praktikum Algo nomor 2.cpp
+++ Jobsheet 5/Jobsheet 5 Praktikum Algo nomor 2.cpp
@@ -1,23 +1,24 @@
-// developed by Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include <stdio.h>
 
 int main() {
+    // deklarasi variabel untuk menyimpan total pembelian dan diskon
     float total_pembelian, diskon;
     
-	// Pengguna diminta menginput total pembelian
+    // Pengguna diminta menginput total pembelian
     printf("Masukkan total pembelian : ");
     scanf("%f", &total_pembelian);
     
     // Mencari diskon menggunakan total pembelian yang diinputkan pengguna
-    if (total_pembelian <= 75000) {
-        diskon = total_pembelian * 0.05;
-    } else if (total_pembelian <= 125000) {
+    if (total_pembelian <= 75000) { // jika total pembelian kurang dari 75000
+        diskon = total_pembelian * 0.05; 
+    } else if (total_pembelian <= 125000) { // jika total pembelian kurang dari 125000
         diskon = total_pembelian * 0.15;
-    } else {
+    } else { // jika total pembelian lebih dari 125000
         diskon = total_pembelian * 0.25 + 5000;
     }
 
-    // Menampilkan Diskon yang diterima dalam rupiah
+    // Menampilkan Diskon yang diterima dalam bentuk rupiah
     printf("Diskon yang diterima: Rp %.2f\n", diskon);
 
     return 0;
