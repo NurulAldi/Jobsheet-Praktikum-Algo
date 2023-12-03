bef36fd1d8e4293c69762ba7ba5eb8bd159a52bd
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-03 09:50:42 +0700

    Update Jobsheet 7  nomor 3 Praktikum Algo.cpp

--- Jobsheet 7/Jobsheet 7  nomor 3 Praktikum Algo.cpp
+++ Jobsheet 7/Jobsheet 7  nomor 3 Praktikum Algo.cpp
@@ -1,8 +1,9 @@
-// developer : Nurul Aldi (23343048)
+// Created by : 23343048_Nurul Aldi
 #include<stdio.h>
 #include<string.h>
 
 int main(){
+	// deklarasi variabel untuk menyimpan password dan username pengguna dengan maksimal karakter sebanyak 50 karakter 
 	char password[50];
 	char username[50];
 	
@@ -11,8 +12,8 @@ int main(){
 	printf("Input password anda : "); // pengguna diminta menginput password
 	scanf(" %s", &password);
 	
-	 if(strcmp(username, "krustak") == 0){
-	 	if(strcmp(password, "wadidaw") == 0){
+	 if(strcmp(username, "krustak") == 0){ // jika username sama dengan "krustak"
+	 	if(strcmp(password, "wadidaw") == 0){ // jika passowrd sama dengan "wadidaw"
 	 		printf("Berhasil login"); // jika password dan username sesuai, maka "Berhasi login" akan ditampilkan 
 		 } else{
 		 	printf("password salah"); // jika password tidak sesuai tapi username sesuai, maka "password salah" akan ditampilkan
