7f0f4b0ffcaf545b5b985c671f62b6aff0778046
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-05 23:25:59 +0700

    Add files via upload

---
+++ Jobsheet 10/Example Jobsheet 10/example a.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+enum hari {SENIN, SELASA, RABU, KAMIS, JUMAT, SABTU, MINGGU};
+
+int main(){
+	enum hari sekarang;
+	sekarang = RABU;
+	printf("Sekarang hari ke-%d", sekarang + 1);
+}
---
+++ Jobsheet 10/Example Jobsheet 10/example d.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+struct student{
+	char name[50];
+	int age;
+};
+
+int display(struct student s){
+	printf("\nDisplaying information\n");
+	printf("Name: %s", s.name);
+	printf("\nRoll: %d", s.age);
+}
+
+int main(){
+	struct student s1;
+	
+	printf("Enter name: ");
+	scanf("%[^\n]%*c", &s1.name);
+	
+	printf("Enter age: ");
+	scanf("%d", &s1.age);
+	
+	display(s1); // passing structure as an argument
+}
+
