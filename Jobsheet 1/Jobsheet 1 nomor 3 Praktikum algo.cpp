9637ddea084bb008807f27fe429d0524be1c2862
Nurul Aldi (23343048) <149251399+NurulAldi@users.noreply.github.com>
2023-12-05 14:41:11 +0700

    Add files via upload

---
+++ Jobsheet 1/Jobsheet 1 nomor 3 Praktikum algo.cpp
@@ -0,0 +1,14 @@
+// Created by : 23343048_Nurul Aldi
+
+// ini adalah library yang dibutuhkan agar fungsi printf() dapat bekerja
+#include<stdio.h>
+
+// ini adalah fungsi main yang akan dieksekusi oleh program
+int main(){
+	// perintah printf() digunakan untuk menampilkan output/hasil ke console
+	printf("Hello world");
+
+}
+
+// kesimpulannya adalah program ini merupakan program sederhana yang 
+// akan menampilkan sebuah kata-kata "Hello world" ke console
